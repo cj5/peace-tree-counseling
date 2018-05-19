ac666c94730e0bebcb5a1f29b8111a57dec5128a
Chris <stac1221@gmail.com>
2018-05-19 07:19:07 -0500

    set up child theme. beginning to structure site

---
+++ wp-content/themes/peace-tree-child/footer-custom.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Peace_Tree
+ */
+
+?>
+
+	<!-- </div> -->
+	<!-- #content -->
+
+	<footer>
+		<div class="container">
+			<p>Peace Tree Counseling</p>
+		</div>
+	</footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/peace-tree-child/template-page.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Peace_Tree
+ */
+
+ /*
+Template Name: Page
+*/
+
+get_header('custom');
+?>
+
+<img class="booger" src="/wp-content/uploads/2018/05/railroad_tracks_through_forest.jpg" alt="">
+
+<div class="content-filler"></div>
+
+<?php
+get_footer('custom');
